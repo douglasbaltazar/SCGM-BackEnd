71815463f58d66fbdfbbcfdae29ec53277ec3b6f
Douglas Baltazar <douglasbaltazar1@gmail.com>
2021-03-02 23:40:24 -0300

    Adição de paginação no Spring

--- src/main/java/com/scgm/backend/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/scgm/backend/repository/lancamento/LancamentoRepositoryQuery.java
@@ -2,10 +2,13 @@ package com.scgm.backend.repository.lancamento;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.scgm.backend.model.Lancamento;
 import com.scgm.backend.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
